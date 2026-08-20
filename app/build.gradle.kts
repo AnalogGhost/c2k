63cf2f6ac0a1764d5e80e310449c4e04a1cbe0dc
ghost <1761407+AnalogGhost@users.noreply.github.com>
2026-08-20 08:45:21 -0600

    Bump version to 1.2.14 (versionCode 21)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.hackerapps.c2k"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.2.13"
+        versionCode = 21
+        versionName = "1.2.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
