9b1cd2698bae6925d04260ce09a1658f8a419ced
Mathew Brown <1761407+AnalogGhost@users.noreply.github.com>
2026-09-01 15:41:39 -0600

    Prepare 1.2.16 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.hackerapps.c2k"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22
-        versionName = "1.2.15"
+        versionCode = 23
+        versionName = "1.2.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
